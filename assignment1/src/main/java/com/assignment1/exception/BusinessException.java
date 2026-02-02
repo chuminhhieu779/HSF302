c05e48aa8cfccac5a11b2671950bf72a51926606
Suzune <124695777+Suzune705@users.noreply.github.com>
2026-02-02 19:07:17 +0700

    chore : changes something

--- assignment1/src/main/java/com/assignment1/exception/BusinessException.java
+++ assignment1/src/main/java/com/assignment1/exception/BusinessException.java
@@ -1,8 +1,18 @@
 package com.assignment1.exception;
 
+import com.assignment1.enums.ErrorCode;
+
 public abstract class BusinessException extends RuntimeException{
-    protected BusinessException(String message) {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode) {
         super(message);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
     }
 }
 
